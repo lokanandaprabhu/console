c120d59e8587b2765f77d3d9117eba39e4a10781
Vikram Raj <vikrmrj@gmail.com>
2023-11-23 19:46:51 +0530

    add archive icon to the PLR and TR fetched from results database

--- frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsRow.tsx
+++ frontend/packages/pipelines-plugin/src/components/taskruns/list-page/TaskRunsRow.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { Tooltip } from '@patternfly/react-core';
+import { ArchiveIcon } from '@patternfly/react-icons';
+import { useTranslation } from 'react-i18next';
 import { TableData, RowFunctionArgs } from '@console/internal/components/factory';
 import { ResourceLink, Timestamp } from '@console/internal/components/utils';
 import { referenceForModel } from '@console/internal/module/k8s';
@@ -13,10 +16,13 @@ import { TektonResourceLabel } from '../../pipelines/const';
 import TaskRunStatus from '../status/TaskRunStatus';
 import { tableColumnClasses } from './taskruns-table';
 
+import './TaskRunsRow.scss';
+
 const taskRunsReference = referenceForModel(TaskRunModel);
 const pipelineReference = referenceForModel(PipelineModel);
 
 const TaskRunsRow: React.FC<RowFunctionArgs<TaskRunKind>> = ({ obj, customData }) => {
+  const { t } = useTranslation();
   const { selectedColumns } = customData;
   return (
     <>
@@ -26,6 +32,17 @@ const TaskRunsRow: React.FC<RowFunctionArgs<TaskRunKind>> = ({ obj, customData }
           name={obj.metadata.name}
           namespace={obj.metadata.namespace}
           data-test-id={obj.metadata.name}
+          nameSuffix={
+            <>
+              {obj?.metadata?.annotations?.['resource.deleted.in.k8s'] === 'true' ? (
+                <Tooltip content={t('pipelines-plugin~Archived in Tekton results')}>
+                  <div className="opp-task-run-list__results-indicator">
+                    <ArchiveIcon />
+                  </div>
+                </Tooltip>
+              ) : null}
+            </>
+          }
         />
       </TableData>
       {selectedColumns?.has('namespace') && (
